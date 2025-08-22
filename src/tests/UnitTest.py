caf46eaaf5936590c04b64a2d3dc3b3e72004432
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-08-22 12:06:20 +0700

    add Unit tests

--- src/tests/UnitTest.py
+++ src/tests/UnitTest.py
@@ -1,25 +1,30 @@
 import os;
 import json;
 import unittest;
+import warnings;
 
-from whisper import Whisper, load_model;
+from whisper import Whisper, load_model, available_models;
 
 class UnitTest(unittest.TestCase):
   model: Whisper
   audio_dir: str
+  models: list[str]
 
   @classmethod
   def setUpClass(cls):
-    cls.model     = load_model(name="large")
+    warnings.filterwarnings("ignore")
+    cls.models    = available_models()
+    cls.model     = load_model(name="large-v3-turbo")
     cls.audio_dir = os.path.join(os.path.dirname(__file__), "audio")
+    print(f"Available models: {cls.models}")
 
   def test_simple_vietnamese(self):
-    result: dict = self.model.transcribe(f"{self.audio_dir}/vietnamese-song.mp3")
+    result: dict = self.model.transcribe(f"{self.audio_dir}/vietnamese.mp3")
     print(json.dumps(result.get("text"), indent=2, ensure_ascii=False))
     self.assertTrue(result.get("text", False))
 
   def test_complex_vietnamese(self):
-    result: dict = self.model.transcribe(f"{self.audio_dir}/vietnamese.mp3")
+    result: dict = self.model.transcribe(f"{self.audio_dir}/vietnamese-song.mp3")
     print(json.dumps(result.get("text"), indent=2, ensure_ascii=False))
     self.assertTrue(result.get("text", False))
 
