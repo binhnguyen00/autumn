669aa14f91086491fae3fe06e0c6f1919c2e3263
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-08-25 15:03:15 +0700

    update backend: can now run debug

--- runtime/backend.sh
+++ runtime/backend.sh
@@ -10,6 +10,7 @@ Usage: ./backend.sh <command>
 
 Available commands:
   start   Start the backend
+  debug   Debug the backend
   stop    Stop the backend
   """
 }
@@ -31,6 +32,16 @@ if [ "$COMMAND" = "start" ]; then
   cd $PROJECT_DIR
   activate_venv
   uvicorn \
+    src.backend.app.Main:app \
+    --host 0.0.0.0 \
+    --port 8080 \
+    --log-level info \
+    --log-config $PROJECT_DIR/src/backend/logs/config.ini \
+    --reload
+elif [ "$COMMAND" = "debug" ]; then
+  cd $PROJECT_DIR
+  activate_venv
+  python -m debugpy --listen 0.0.0.0:5678 -m uvicorn \
     src.backend.app.Main:app \
     --host 0.0.0.0 \
     --port 8080 \
